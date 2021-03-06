b8f4cd4543ee6db2beaebd2cf9ec0b25082de6e8
vincent <32922167+V-vincent@users.noreply.github.com>
2021-03-06 17:17:58 +0800

    add test 01 02

---
+++ 03-test/01.js
@@ -0,0 +1,41 @@
+var a = 1;
+
+function change1(a) {
+  a = 2;
+}
+change1(a);
+console.log(a); // 1
+
+var user = {
+  age: 10,
+}
+
+function change2(user) {
+  user.age = 20;
+}
+change2(user);
+console.log(user.age); // 20
+function change3(user) {
+  user = {
+    age: 30
+  }
+}
+change3(user);
+console.log(user.age); // 20
+
+function test(a, b) {
+  console.log(b);
+  return {
+    test: function (c, a) {
+      return test(c, a);
+    }
+  }
+}
+var b = test(100, 200); // 200
+b.test(300); // undefined
+b.test(400); // undefined
+
+var c = test(101).test(201).test(401); // undefined undefined undefined
+
+var d = test(102).test(202, 302); // undefined 302
+d.test(); // undefined
---
+++ 03-test/02.js
@@ -0,0 +1,33 @@
+// 拦截代理
+// 如何让 e == 5 && e == 8 
+var e = {
+  i: 0,
+  // valueOf: function () {
+  //   return this.i++ * 3 + 5;
+  // },
+  toString: function () {
+    return this.i++ * 3 + 5;
+  },
+}
+console.log(e == 5 && e == 8);
+
+var f = [5, 8];
+f.join = f.shift;
+console.log(f == 5 && f == 8);
+
+var g = new Proxy({
+  i: 0,
+}, {
+  get(target) {
+    return () => target.i++ * 3 + 5;
+  }
+});
+console.log(g == 5 && g == 8);
+
+var h = 0;
+Object.defineProperty(window, 'i', {
+  get: function () {
+    return h++ * 3 + 5;
+  },
+})
+console.log(i === 5 && i === 8);
