7eeae52d18648fdb1ff20b9146a783191c038d6c
vincent <1131182508@qq.com>
2021-04-06 10:49:59 +0800

    Create eventBus.js

---
+++ 02-tool/eventBus.js
@@ -0,0 +1,41 @@
+class Events {
+  constructor() {
+    this.events = new Map();
+  }
+
+  addEvent(key, fn, isOnce, ...args) {
+    const value = this.events.get(key) ? this.events.get(key) : this.events.set(key, new Map()).get(key)
+    value.set(fn, (...args1) => {
+      fn(...args, ...args1)
+      isOnce && this.off(key, fn)
+    })
+  }
+
+  on(key, fn, ...args) {
+    if (!fn) {
+      console.error(`没有传入回调函数`);
+      return
+    }
+    this.addEvent(key, fn, false, ...args)
+  }
+
+  fire(key, ...args) {
+    if (!this.events.get(key)) {
+      console.warn(`没有 ${key} 事件`);
+      return;
+    }
+    for (let [, cb] of this.events.get(key).entries()) {
+      cb(...args);
+    }
+  }
+
+  off(key, fn) {
+    if (this.events.get(key)) {
+      this.events.get(key).delete(fn);
+    }
+  }
+
+  once(key, fn, ...args) {
+    this.addEvent(key, fn, true, ...args)
+  }
+}
