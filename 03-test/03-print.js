42251f3b7f8607227dcd11ae74f85e6acb8fe6cd
vincent <1131182508@qq.com>
2021-04-13 18:08:15 +0800

    对象的push属性为数组的push方法时

--- 03-test/03-print.js
+++ 03-test/03-print.js
@@ -34,11 +34,30 @@
 //   console.log('500')
 // }, 500)
 
-var a = 10;
-(function () {
-  console.log(a); // undefined
-  a = 5
-  console.log(window.a); // 10
-  var a = 20;
-  console.log(a); // 20
-})()
+// var a = 10;
+// (function () {
+//   console.log(a); // undefined
+//   a = 5
+//   console.log(window.a); // 10
+//   var a = 20;
+//   console.log(a); // 20
+// })()
+
+// 输出以下代码执行的结果并解释为什么
+var obj = {
+  '2': 3,
+  '3': 4,
+  'length': 2,
+  'splice': Array.prototype.splice,
+  'push': Array.prototype.push
+}
+obj.push(1)
+obj.push(2)
+console.log(obj);
+console.log(JSON.stringify(obj));
+// 输出一个伪数组: `[empty × 2, 1, 2, splice: ƒ, push: ƒ]`
+// `push()` 方法将一个或多个元素添加到数组的末尾，并返回该数组的新长度
+// 第一次`push`，`obj`对象的长度为`2`，于是`obj[2] = 1`，`obj.length += 1`
+// 第二次`push`，`obj`对象的长度为`3`，于是`obj[3] = 1`，`obj.length += 1`
+// 使用`console.log`输出的时候，因为 `obj` 具有 `length` 属性和 `splice` 方法，故将其作为数组进行打印
+// 打印时因为数组未设置下标为 `0` `1` 处的值，故打印为`empty`，主动 `obj[0]` 获取为 `undefined`
