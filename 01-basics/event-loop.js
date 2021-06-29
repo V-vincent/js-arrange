3b622ea53b3fe4ca648533af23bd431f9e134a08
vincent <1131182508@qq.com>
2021-06-29 17:07:37 +0800

    Create event-loop.js

---
+++ 01-basics/event-loop.js
@@ -0,0 +1,44 @@
+// setTimeout(() => {
+//   console.log('timer1')
+//   Promise.resolve().then(function () {
+//     console.log('promise1')
+//   })
+// }, 0)
+// setTimeout(() => {
+//   console.log('timer2')
+//   Promise.resolve().then(function () {
+//     console.log('promise2')
+//   })
+// }, 0)
+
+// Promise.resolve().then(() => {
+//   console.log('Promise1')
+//   setTimeout(() => {
+//     console.log('setTimeout1')
+//   }, 0)
+// })
+// setTimeout(() => {
+//   console.log('setTimeout2')
+//   Promise.resolve().then(() => {
+//     console.log('Promise2')
+//   })
+// }, 0)
+
+// console.log('start')
+// setTimeout(() => {
+//   console.log('timer1')
+//   Promise.resolve().then(function () {
+//     console.log('promise1')
+//   })
+// }, 0)
+// setTimeout(() => {
+//   console.log('timer2')
+//   Promise.resolve().then(function () {
+//     console.log('promise2')
+//   })
+// }, 0)
+// Promise.resolve().then(function () {
+//   console.log('promise3')
+// })
+// console.log('end')
+
