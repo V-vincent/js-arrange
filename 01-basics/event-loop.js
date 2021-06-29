cdd5fee6802bedad18b6b3c528dbd252dce7e66f
vincent <32922167+V-vincent@users.noreply.github.com>
2021-06-29 23:57:11 +0800

    Update event-loop.js

--- 01-basics/event-loop.js
+++ 01-basics/event-loop.js
@@ -1,160 +1,189 @@
 // 基础版本
 // setTimeout
-// console.log('script start');
 // setTimeout(function () {
-//   console.log('setTimeout')
-// }, 0)
+//   console.log('setTimeout1')
+// }, 100)
+// setTimeout(function () {
+//   console.log('setTimeout2')
+// }, 50)
 // console.log('script end');
 
 // promise
 // console.log('script start');
-// Promise.resolve().then(function () {
-//   console.log('promise');
+// new Promise(function (resolve) {
+//   console.log('promise1');
+//   resolve();
+//   console.log('promise1 end');
+// }).then(function () {
+//   console.log('promise2');
 // })
+
 // console.log('script end');
 
 // async/await
 // async function async1() {
 //   console.log('async1 start')
-//   await 1;
+//   await async2();
 //   console.log('async1 end')
+// }
+// async function async2() {
+//   console.log('async2')
 // }
 // console.log('script start')
 // async1();
 // console.log('script end')
 
-// 单个任务进化版本
-
-
-// setTimeout(() => {
-//   console.log('timer1')
-//   Promise.resolve().then(function () {
-//     console.log('promise1')
-//   })
-// }, 0)
-// setTimeout(() => {
-//   console.log('timer2')
-//   Promise.resolve().then(function () {
-//     console.log('promise2')
-//   })
-// }, 0)
-
-// Promise.resolve().then(() => {
-//   console.log('Promise1')
-//   setTimeout(() => {
-//     console.log('setTimeout1')
-//   }, 0)
-// })
-// setTimeout(() => {
-//   console.log('setTimeout2')
-//   Promise.resolve().then(() => {
-//     console.log('Promise2')
-//   })
-// }, 0)
-
-// console.log('start')
-// setTimeout(() => {
-//   console.log('timer1')
-//   Promise.resolve().then(function () {
-//     console.log('promise1')
-//   })
-// }, 0)
-// setTimeout(() => {
-//   console.log('timer2')
-//   Promise.resolve().then(function () {
-//     console.log('promise2')
-//   })
-// }, 0)
-// Promise.resolve().then(function () {
-//   console.log('promise3')
-// })
-// console.log('end')
-
-// console.log('打印' + 10);
-// new Promise(function (resolve, reject) {
-//   setTimeout(function () {
-//     console.log('打印' + 5);
-//   });
-// }).then(
-//   console.log('打印' + 6));
+// 组合版本一
+// console.log('script start');
 // setTimeout(function () {
-//   new Promise(function (resolve, reject) {
-//     console.log('打印' + 7);
-//   });
-// })
-
-// console.log('打印' + 1);
-// setTimeout(function () {
-//   console.log('打印' + 2);
-// })
+//   console.log('setimeout');
+// }, 0)
 // new Promise(function (resolve) {
-//   console.log('打印' + 3);
+//   console.log('promise1');
 //   resolve();
+//   console.log('promise1 end');
 // }).then(function () {
-//   console.log(4);
-// }
-// );
-// console.log('打印' + 10);
-// new Promise(function (resolve) {
-//   setTimeout(function () {
-//     console.log('打印' + 5);
-//   });
-//   resolve();
-// }).then(function () {
-//   console.log('打印' + 6)
-// });
-// setTimeout(function () {
-//   new Promise(function (resolve) {
-//     console.log('打印' + 7);
-//   });
+//   console.log('promise2');
 // })
+// console.log('script end');
 
-// console.log('start')
-// setTimeout(() => {
-//   console.log('timer1')
-//   Promise.resolve().then(function () {
-//     console.log('promise1')
-//   })
-// }, 0)
-// setTimeout(() => {
-//   console.log('timer2')
-//   Promise.resolve().then(function () {
-//     console.log('promise2')
-//   })
-// }, 0)
-// Promise.resolve().then(function () {
-//   console.log('promise3')
-// })
-// console.log('end')
-
+// 组合版本二
 // console.log('script start')
-
 // async function async1() {
+//   console.log('async1 start')
 //   await async2()
 //   console.log('async1 end')
 // }
 // async function async2() {
-//   console.log('async2 end')
+//   console.log('async2')
 // }
 // async1()
-
-// setTimeout(function () {
-//   console.log('setTimeout')
-// }, 0)
-
 // new Promise(resolve => {
-//   console.log('Promise')
+//   console.log('promise1 start')
 //   resolve()
+//   console.log('promise1 end')
+// }).then(function () {
+//   console.log('promise2')
 // })
-//   .then(function () {
-//     console.log('promise1')
-//   })
-//   .then(function () {
-//     console.log('promise2')
-//   })
-
 // console.log('script end')
 
+// 混合版本一
+// console.log('script start')
+// setTimeout(() => {
+//   console.log('setTimeout1')
+//   Promise.resolve().then(function () {
+//     console.log('promise1')
+//   })
+// }, 0)
+// setTimeout(() => {
+//   console.log('setTimeout2')
+//   Promise.resolve().then(function () {
+//     console.log('promise2')
+//   })
+// }, 0)
+// console.log('script end')
+
+// 混合版本二
+// console.log('script start')
+// Promise.resolve().then(() => {
+//   console.log('promise1')
+//   setTimeout(() => {
+//     console.log('setTimeout1')
+//   }, 0)
+// })
+// Promise.resolve().then(() => {
+//   console.log('promise2')
+//   setTimeout(() => {
+//     console.log('setTimeout2')
+//   }, 0)
+// })
+// Promise.resolve().then(function () {
+//   console.log('promise3')
+// })
+// console.log('script end')
+
+// 混合版本三
+// async function async1() {
+//   console.log('async1 start');
+//   Promise.resolve(async2()).then(() => {
+//     console.log('async1 end');
+//   })
+// }
+// async function async2() {
+//   console.log('async2');
+//   Promise.resolve(async3()).then(() => {
+//     console.log('async2 end');
+//   })
+// }
+// async function async3() {
+//   console.log('async3');
+//   Promise.resolve().then(() => {
+//     console.log('async3 end');
+//   })
+// }
+// console.log('script start');
+// async1();
+// new Promise(function (resolve) {
+//   console.log('promise1');
+//   resolve();
+// }).then(function () {
+//   console.log('promise2');
+// });
+// console.log('script end');
+
+// 混合版本四
+// async function async1() {
+//   console.log('async1 start');
+//   await async2();
+//   console.log('async1 end');
+// }
+// async function async2() {
+//   console.log('async2');
+//   await async3()
+//   console.log('async2 end')
+// }
+// async function async3() {
+//   await console.log('async3');
+//   console.log('async3 end')
+// }
+// console.log('script start');
+// async1();
+// new Promise((resolve) => {
+//   console.log('promise1');
+//   resolve();
+// }).then(() => {
+//   console.log('promise2');
+// });
+// console.log('script end');
+
+// 混合版本五
+async function async1() {
+  console.log('async1 start');
+  Promise.resolve(async2()).then(() => {
+    console.log('async1 end');
+  })
+}
+async function async2() {
+  console.log('async2');
+  Promise.resolve(async3()).then(() => {
+    console.log('async2 end');
+  })
+}
+async function async3() {
+  await console.log('async3');
+  console.log('async3 end');
+}
+console.log('script start');
+async1();
+new Promise(function (resolve) {
+  console.log('promise1');
+  resolve();
+}).then(function () {
+  console.log('promise2');
+});
+console.log('script end');
+
+// setTimeout和setImmediate
 // setTimeout(() => {
 //   console.log('timeout');
 // }, 0)
@@ -187,24 +216,3 @@
 //     console.log("nextTick");
 //   })
 // });
-
-// console.log('start')
-// setTimeout(() => {
-//   console.log('timer1')
-//   Promise.resolve().then(function () {
-//     console.log('promise1')
-//   })
-// }, 0)
-// setTimeout(() => {
-//   console.log('timer2')
-//   Promise.resolve().then(function () {
-//     console.log('promise2')
-//   })
-// }, 0)
-// Promise.resolve().then(function () {
-//   console.log('promise3')
-// })
-// console.log('end')
-
-// https://zhuanlan.zhihu.com/p/54882306
-// https://blog.csdn.net/qq_41257129/article/details/100743394
