1cb21fb74f1c997a8df8fc270084a3e6f8a55daf
vincent <1131182508@qq.com>
2021-06-30 17:54:58 +0800

    Update event-loop.js

--- 01-basics/event-loop.js
+++ 01-basics/event-loop.js
@@ -217,27 +217,27 @@ console.log('script end');
 //   })
 // });
 
-
-function func1() {
-  console.log('func1 start');
-  return new Promise(resolve => { resolve('OK'); })
-}
-function func2() {
-  console.log('func2 start');
-  return new Promise(resolve => { setTimeout(() => { resolve('OK'); }, 10) })
-}
-console.log(1);
-setTimeout(async () => {
-  console.log(2);
-  await func1();
-  console.log(3);
-}, 20);
-for (let i = 0; i < 90000000; i++) { } // 约 80 ms
-console.log(4);
-func1().then(() => { console.log(5); })
-func2().then(() => { console.log(6); })
-setTimeout(() => { console.log(7) }, 0); 
-console.log(8);
+// 网友拓展题
+// function func1() {
+//   console.log('func1 start');
+//   return new Promise(resolve => { resolve('OK'); })
+// }
+// function func2() {
+//   console.log('func2 start');
+//   return new Promise(resolve => { setTimeout(() => { resolve('OK'); }, 10) })
+// }
+// console.log(1);
+// setTimeout(async () => {
+//   console.log(2);
+//   await func1();
+//   console.log(3);
+// }, 20);
+// for (let i = 0; i < 90000000; i++) { } // 约 80 ms
+// console.log(4);
+// func1().then(() => { console.log(5); })
+// func2().then(() => { console.log(6); })
+// setTimeout(() => { console.log(7) }, 0); 
+// console.log(8);
 
 // 1
 // 4
