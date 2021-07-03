08c97516262af82ba0cdcfdbb88b015e0fc76db3
vincent <32922167+V-vincent@users.noreply.github.com>
2021-07-03 20:35:00 +0800

    setTimeout准时吗？

--- 01-basics/10-setTimeout.js
+++ 01-basics/10-setTimeout.js
@@ -9,3 +9,30 @@ let timer = setTimeout(function() {
   console.log('1 秒后打印');
 }, 1000)
 console.log(timer);
+
+// let startTime = +new Date();
+// for (let i = 0; i < 90000000; i++) { } // 约 50 ms
+// let endTime = +new Date();
+// console.log(endTime - startTime);
+
+// let startTime = +new Date();
+// setTimeout(() => {
+//   console.log(1);
+//   let endTime = +new Date();
+//   console.log(endTime - startTime);
+// }, 20);
+// for (let i = 0; i < 90000000; i++) { } 
+// let startTime2 = +new Date();
+// setTimeout(() => {
+//   console.log(2);
+//   let endTime2 = +new Date();
+//   console.log(endTime2 - startTime2);
+// }, 0);
+
+setTimeout(() => {
+  console.log(1);
+}, 20);
+for (let i = 0; i < 9000000; i++) { } 
+setTimeout(() => {
+  console.log(2);
+}, 0);
