0b40bbff82b7b6057ad44ba04bdac38a526a6e38
vincent <1131182508@qq.com>
2021-07-05 09:46:00 +0800

    Update 06-concurrent.js

--- 04-test/06-concurrent.js
+++ 04-test/06-concurrent.js
@@ -1,14 +1,28 @@
 // JS 实现一个带并发限制的异步调度器 Scheduler，
 // 保证同时运行的任务最多有两个。完善代码中 Scheduler 类，使得以下程序能正确输出。
 
+// finally() 方法返回一个Promise。在promise结束时，无论结果是fulfilled或者是rejected，都会执行指定的回调函数。这为在Promise是否成功完成后都需要执行的代码提供了一种方式。
 class Scheduler {
   constructor() {
-    this.list = [];
+    this.taskList = [];
     this.curTask = 0;
   }
+  // async add(fn) {
+  //   if (this.curTask >= 2) {
+  //     await new Promise(resolve => this.taskList.push(resolve));
+  //   }
+  //   this.curTask++;
+  //   return await fn().finally(() => {
+  //     this.curTask--;
+  //     if (this.taskList.length) {
+  //       this.taskList[0]();
+  //       this.taskList.shift();
+  //     }
+  //   })
+  // }
   async add(promiseCreator) {
     if (this.curTask >= 2) {
-      await new Promise(resolve => this.list.push(resolve))
+      await new Promise(resolve => this.taskList.push(resolve))
     }
     return this.handlerPromise(promiseCreator);
   }
@@ -20,9 +34,9 @@ class Scheduler {
       return Promise.reject(err);
     } finally {
       this.curTask--;
-      if (this.list.length) {
-        this.list[0]();
-        this.list.shift();
+      if (this.taskList.length) {
+        this.taskList[0]();
+        this.taskList.shift();
       }
     }
   }
