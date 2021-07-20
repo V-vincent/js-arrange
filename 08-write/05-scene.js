1659feab45775b38c716abe37ac2d0236ebdd332
vincent <32922167+V-vincent@users.noreply.github.com>
2021-07-20 23:01:20 +0800

    Create 05-scene.js

---
+++ 08-write/05-scene.js
@@ -0,0 +1,28 @@
+// 某公司 1 到 12 月份的销售额存在一个对象里面
+// 如下：{1:222, 2:123, 5:888}，请把数据处理为如下结构：[222, 123, null, null, 888, null, null, null, null, null, null, null]。
+function dealSaleDate(obj) {
+  if (toString.call(obj) !== '[object Object]') {
+    throw new Error('this obj is not an object');
+  }
+  let arr = Array(12).fill(null);
+  for (let i = 0; i < 12; i++) {
+    arr[i] = obj[i + 1] || null
+  }
+  return arr;
+}
+function dealSaleObjToArr(obj) {
+  return Array.from({ length: 12 }).map((item, index) => obj[index + 1] || null);
+}
+// console.log(dealSaleObjToArr({ 1: 222, 2: 123, 5: 888 }))
+
+// url有三种情况
+// https://www.xx.cn/api?keyword=&level1=&local_batch_id=&elective=&local_province_id=33
+// https://www.xx.cn/api?keyword=&level1=&local_batch_id=&elective=800&local_province_id=33
+// https://www.xx.cn/api?keyword=&level1=&local_batch_id=&elective=800,700&local_province_id=33
+// 匹配elective后的数字输出（写出你认为的最优解法）:
+// [] || ['800'] || ['800','700']
+function getUrlValue(url) {
+  if (!url) return;
+  let res = url.match(/(?<=elective=)(\d+(,\d+)*)/);
+  return res ? res[0].split(',') : [];
+}
