58513ccc7df2891153fb0c30b8d293823926d193
vincent <32922167+V-vincent@users.noreply.github.com>
2021-07-20 23:59:57 +0800

    Update 04-realize.js

--- 08-write/04-realize.js
+++ 08-write/04-realize.js
@@ -63,4 +63,36 @@ function continuityNum(str) {
   return res.join(',');
 }
 
-// 
+// 实现一个 compose 函数
+// 用法如下:
+function fn1(x) {
+  return x + 1;
+}
+function fn2(x) {
+  return x + 2;
+}
+function fn3(x) {
+  return x + 3;
+}
+function fn4(x) {
+  return x + 4;
+}
+const com = compose(fn1, fn2, fn3, fn4);
+// console.log(com(1)); // 1 + 4 + 3 + 2 + 1 = 11
+// 分析 compose 返回一个函数，里面按后入先出的顺序执行
+function compose(...fn) {
+  return (num) => {
+    return fn.reverse().reduce((pre, cur) => {
+      return cur(pre);
+    }, num)
+  }
+}
+// function compose(...fn) {
+//   if (!fn.length) return (v) => v;
+//   if (fn.length === 1) return fn[0];
+//   return fn.reduce(
+//     (pre, cur) =>
+//       (...args) =>
+//         pre(cur(...args))
+//   );
+// }
