128b50e0cf9ca6a9b62c0965cd39e2fa597c8588
vincent <32922167+V-vincent@users.noreply.github.com>
2021-08-19 14:19:49 +0800

    Changes

--- 01-basics/10-setTimeout.js
+++ 01-basics/10-setTimeout.js
@@ -10,10 +10,10 @@ let timer = setTimeout(function () {
 }, 1000)
 console.log(timer);
 
-let startTime = +new Date();
-for (let i = 0; i < 90000000; i++) { } // 约 50 ms
-let endTime = +new Date();
-console.log(endTime - startTime);
+// let startTime = +new Date();
+// for (let i = 0; i < 90000000; i++) { } // 约 50 ms
+// let endTime = +new Date();
+// console.log(endTime - startTime);
 
 // let startTime = +new Date();
 // setTimeout(() => {
@@ -38,13 +38,13 @@ console.log(endTime - startTime);
 // }, 0);
 
 
-var name = "我是全局的变量";
-var user = {
+let name = "我是全局的变量";
+let user = {
   name: "我是对象里面的变量",
   showName: function () {
     console.log(this.name);
   }
-}
+};
 setTimeout(user.showName, 1000);
 
 // 第一种是将 user.showName 放在匿名函数中执行
@@ -57,16 +57,16 @@ setTimeout(() => user.showName(), 1000);
 // 第二种是使用 call/apply/bind 等方法，改变 showName 的 this 指向
 setTimeout(user.showName.call(user), 1000)
 
-var name = "我是全局的变量";
-var user = {
-  name: "我是对象里面的变量",
-  showName: function () {
-    setTimeout(function () {
-      console.log(this.name);
-    }, 0)
-  }
-}
-user.showName();
+// var name = "我是全局的变量";
+// var user = {
+//   name: "我是对象里面的变量",
+//   showName: function () {
+//     setTimeout(function () {
+//       console.log(this.name);
+//     }, 0)
+//   }
+// }
+// user.showName();
 
 // 箭头函数
 // var name = "我是全局的变量";
@@ -147,12 +147,12 @@ setTimeout(function () {
 }, 0)
 
 
-let startTime = +new Date();
-function cb() {
-  console.log(+new Date() - startTime)
-  setTimeout(cb, 0);
-}
-setTimeout(cb, 0);
+// let startTime = +new Date();
+// function cb() {
+//   console.log(+new Date() - startTime)
+//   setTimeout(cb, 0);
+// }
+// setTimeout(cb, 0);
 
 setTimeout(() => {
   setTimeout(() => {
